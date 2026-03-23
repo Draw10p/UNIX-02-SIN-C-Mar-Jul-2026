85ca4de2e0ecb446d909be18c62c93f2779f665c
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-03-23 14:51:56 +0000

    Apunte 1 - Clase 23 -3 -2026 9.46

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,5 +1,8 @@
-# uname -a (Conocer que sistema esta instalado)
-# which gpg (Comprobar Instalación)
-# gpg --version(Conocer version de GPG)
-# gpg --full-generate-key ( Generar Par LLaves)
-# gpg --list-keys (llavero digital)
+# uname -a (Check which system is installed)
+# which gpg (Check the installation)
+# gpg --version(Check the GPG version)
+# gpg --full-generate-key (Generate a key pair)
+# gpg --list-keys (Lists the keyring)
+# gpg --armor --export (This command exports the public key)
+# gpg --list-secret-keys --keyid-format=long ( This command lists the private keys)
+# hash copied from the private key A13BBAAB40C5844C
