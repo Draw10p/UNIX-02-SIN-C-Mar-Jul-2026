e5df39adfd0e36908467c93129f3a3510a4384ab
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-03-27 15:18:36 +0000

    wip: Coding in C - Class March 27,2026 10:18

--- fylesystem_script.sh
+++ fylesystem_script.sh
@@ -1,6 +1,17 @@
 # cd /dev (" Change directory to dev directory")
 # ls (" list archives and directories ")
+# Partitioning disks in Linux: SDA and SDB 
+#------------------------------------------------------------------------------------------
 # ^C  ("is used to terminate or stop any running process  ")
 # cd /proc (" Change directory to proc directory") 
 # ls ("list procces in proc directory")
 # cat cpuinfo (" show cpu information")
+#------------------------------------------------------------------------------------------
+# cd / ("This command helps us move to the root directory of the filesystem")
+# ls  ("This command helps us list the files and directories in the current directory")
+#------------------------------------------------------------------------------------------
+# cd /bin ("This command helps us move to the bin directory, which contains essential command binaries that are necessary for the system to function properly")
+# ls ("This command helps us list the files and directories in the current directory, which in this case is the bin directory")
+#------------------------------------------------------------------------------------------
+# cd/workspaces/UNIX-02-SIN-C-Mar-Jul-2026 ("This command helps us move to the directory where we have our codespace")
+# gcc saludo.c -o saludo ("This command compiles the C file named saludo.c and creates an executable file named saludo")
