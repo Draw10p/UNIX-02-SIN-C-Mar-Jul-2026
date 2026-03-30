316242009ff785f596f4f6726b5da7cb1b6f562b
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-03-30 15:17:52 +0000

    wip: Installing Libary numpy - Class March 30, 2026 10:17

--- parted_script.sh
+++ parted_script.sh
@@ -1,3 +1,4 @@
 sudo apt update #(It updates the local list of available software packages and their latest versions from the repositories)
 sudo apt upgrade #(It downloads and installs the newest available versions of all software packages currently installed on your system.)
-sudo apt isntall parted #(It downloads and installs the parted software package)
+sudo apt install parted #(It downloads and installs the parted software package)
+pip install numpy #(It uses Python's package installer (pip) to download and install the numpy library)
