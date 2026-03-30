54f92b66a776681e66f94c9dcb848084115a567f
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-03-30 15:14:29 +0000

    wip: Installing Parted in the Terminal - Class March 30, 2026 10:14

---
+++ parted_script.sh
@@ -0,0 +1,3 @@
+sudo apt update #(It updates the local list of available software packages and their latest versions from the repositories)
+sudo apt upgrade #(It downloads and installs the newest available versions of all software packages currently installed on your system.)
+sudo apt isntall parted #(It downloads and installs the parted software package)
