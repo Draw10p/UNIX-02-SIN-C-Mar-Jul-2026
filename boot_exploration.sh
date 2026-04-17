2108d6406c4a813f6092f16f3487fc016bf14a86
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-04-17 14:26:06 +0000

    wip: Executing ls -a and ls -all - Darwin Román 17 / 4 / 2026

---
+++ boot_exploration.sh
@@ -0,0 +1,20 @@
+ls -a #lists all files and folders in the current directory, including hidden ones that start with a dot
+@Draw10p ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot_exploration) $ ls -a
+.                    doc_cifrado_y_firmado.txt                        keyla_doc_cifrado_y_firmado.txt
+..                   doc_firmado_y_firmado_descifrado_y_validado.txt  keyla_doc_no_cifrado.txt
+.git                 doc_no_cifrado.txt                               keyla_doc_no_cifrado_firmado.txt
+.gitignore           doc_no_cifrado_firmado.txt                       keyla_doc_no_cifrado_firmado_binario.txt
+LICENSE              doc_no_cifrado_firmado_binario.txt               keyla_firma_separada_doc_no_cifrado.sig
+README.md            firma_separada_doc_no_cifrado.sig                keyla_llave.publica.asc
+boot_exploration.sh  gnupg_script.sh                                  mi_llave_publica.asc
+doc_cifrado.txt      keyla_doc_cifrado.txt
+#----------------------------------------------------------------------------------------------------
+ls - all # It does the same thing as the previous command, but the command line is longer.
+.                    doc_cifrado_y_firmado.txt                        keyla_doc_cifrado_y_firmado.txt
+..                   doc_firmado_y_firmado_descifrado_y_validado.txt  keyla_doc_no_cifrado.txt
+.git                 doc_no_cifrado.txt                               keyla_doc_no_cifrado_firmado.txt
+.gitignore           doc_no_cifrado_firmado.txt                       keyla_doc_no_cifrado_firmado_binario.txt
+LICENSE              doc_no_cifrado_firmado_binario.txt               keyla_firma_separada_doc_no_cifrado.sig
+README.md            firma_separada_doc_no_cifrado.sig                keyla_llave.publica.asc
+boot_exploration.sh  gnupg_script.sh                                  mi_llave_publica.asc
+doc_cifrado.txt      keyla_doc_cifrado.txt
