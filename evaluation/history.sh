e94289b3145c75ba1133330c880b576a63eeede4
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-04-24 15:00:58 +0000

    wip: History - Darwin Román 24/4/2026

---
+++ evaluation/history.sh
@@ -0,0 +1,24 @@
+    1  ls -l
+    2  notas/
+    3  orion/
+    4  clear
+    5  cd evaluation/orion
+    6  mv notas apuntes
+    7  mkdir respaldos
+    8  mv telemetria.log apuntes/
+    9  mv config.tmp config.conf
+   10  chmod 640 apuntes/telemetria.log
+   11  ls -l apuntes/telemetria.log
+   12  chmod u+x script.sh
+   13  chmod o-w config.conf
+   14  ls -l
+   15  chmod u+s script.sh
+   16  ls -l script.sh
+   17  chmod +t /tmp/orion_zone
+   18  mkdir /tmp/orion_zone
+   19  chmod 777 /tmp/orion_zone
+   20  ls -ld /tmp/orion_zone
+   21  gpg --gen-key
+   22  gpg --encrypt --recipient lyra@orion.lab apuntes/telemetria.log
+   23  ls -l apuntes/
+   24  history
