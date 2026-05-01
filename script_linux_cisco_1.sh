066bbbfe0339b5c475905c5dc1787b3a74edcc83
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-05-01 18:30:03 +0000

    wip: Running the three command in the Cisco course - Darwin Román 1 / 5 / 2026

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,2 +1,3 @@
 1. ls # This returns a list of the files in the current directory.
 2. aptitude moo # This command is a package management feature available in some versions of Linux, which accepts “moo” as an argument
+3. ls -l # This lists the contents of a directory in long format and provides detailed information about each file
