d2af17444a64d0ea369d3d1468ad4bf849aae945
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-05-08 14:38:38 +0000

    wip: commands about topic of groups in Linux - Darwin Román 8 - 05 -2026

---
+++ security.sh
@@ -0,0 +1,6 @@
+1. id # This command prints the user and group identity information (UID, GID, and groups) for the current user.
+# [Darwin-Román✝] UNIX-02-SIN-C-Mar-Jul-2026 √ # id
+#  uid=0(root) gid=0(root) grupos=0(root) 
+2. groups # Command prints the names of all the primary and secondary groups that the current user belongs to.
+3. cat /etc/passwd # command outputs the complete, unfiltered contents of the system's user account file to the terminal.
+4. cat /etc/passwd | head -10  #This command outputs the first 10 lines of the /etc/passwd file, which stores system user account information.
