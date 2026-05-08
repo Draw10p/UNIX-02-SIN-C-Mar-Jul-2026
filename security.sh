8436f3c3f18501482ff3c60930f799a050244dcb
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-05-08 14:43:25 +0000

    wip: commands about the inheritance of my files and directories  - Darwin Román 8 - 05 -2026

--- security.sh
+++ security.sh
@@ -4,3 +4,4 @@
 2. groups # Command prints the names of all the primary and secondary groups that the current user belongs to.
 3. cat /etc/passwd # command outputs the complete, unfiltered contents of the system's user account file to the terminal.
 4. cat /etc/passwd | head -10  #This command outputs the first 10 lines of the /etc/passwd file, which stores system user account information.
+5. touch test.txt / ls -la # My files and directories inherit the GID and UID
