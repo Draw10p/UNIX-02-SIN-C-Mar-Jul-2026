472d9861a242fe1ea49a3e687a1e2aaa2857e83e
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-06-01 15:06:11 +0000

    wip: The Shebang Line  - Darwin Román - 1 / 6 / 2026

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,14 @@
-bash -- version 
+#!/bin/bash
+#!/usr/bin/env bash
+#!/bin/bash -x
+# bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -e -f
+ps -ef
+df --human-readable
+# bash -x blackhatbash1.sh
+# bash -r blackhatbash1.sh
