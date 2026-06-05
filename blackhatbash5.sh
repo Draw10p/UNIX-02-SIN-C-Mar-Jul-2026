4cc11f397916f261ff0553a065b0cc518ca9f40c
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-06-05 15:38:16 +0000

    wip:   Redirection Operators - Darwin Román - 5 / 6 / 2026

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,16 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El commando lzl fallo"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
+echo "Goodbye!" >> output.txt
+cat output.txt  
+
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+lzl 2> error.txt
