607ff0c3f5ffd4134a8fb266f0b08a6697f1ab59
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-06-09 23:44:51 +0000

    wip:  modife in string_comparison  - Darwin Román - 9/ 6 / 2026

--- string_comparison.sh
+++ string_comparison.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 VARIABLE_ONE="nostarch"
 VARIABLE_TWO="nostarch"
-if [["${VARIABLE_ONE}" == "${VARIABLE_TWO}"]]; then
-echo "They are equal!"
+
+# Test that the two variables are equal
+if [[ "${VARIABLE_ONE}" == "${VARIABLE_TWO}" ]]; then
+  echo "They are equal!"
 else
-echo "They are not equal!"
+  echo "They are not equal!"
 fi
