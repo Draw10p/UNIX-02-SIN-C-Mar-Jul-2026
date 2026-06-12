ada67e617d9cea914eacd05a2a4a53cbf9dbd40d
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-06-12 15:07:56 +0000

    wip: function of until loop - Darwin Román - 12 / 6 / 2026

---
+++ until_loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the filename to check
+FILE="output.txt"
+# Create the file if it doesn't exist
+touch "${FILE}"
+# Loop until the file has content (size > 0)
+until [[ -s "${FILE}" ]]; do
+	# Display that the file is empty
+	echo "${FILE} is empty..."
+	# Notify user to check again in 2 seconds
+	echo "Checking again in 2 seconds..."
+	# Wait 2 seconds before checking again
+	sleep 2
+done
+# When file has content, display success message
+echo "${FILE} appears to have some content in it!"
