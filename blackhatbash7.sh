606545643e7296417679afd742d3b56c36d27f47
DARWIN OSWALDO ROMAN AGUIRRE <daromanag@uide.edu.ec>
2026-06-19 15:50:51 +0000

    wip: function of sed changes  - Darwin Román - 19 / 6 / 2026

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -42,3 +42,15 @@ grep "42.236.10.117" log.txt | awk '{print $7}' # This command first filters the
 sed 's/Mozilla/Godzilla/g' log.txt
 
 sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+
+sed 's/ //g' log.txt
+
+sed '1d' log.txt
+
+sed '$d' log.txt
+
+sed '5,7d' log.txt
+
+sed -n '2,15 p' log.txt
+
+sed -i '1d' log.txt
